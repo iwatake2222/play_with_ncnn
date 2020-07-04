c4db21afe7a95dfe9e385280c3772760f9ca6f83
take-iwiw <take.iwiw2222@gmail.com>
2020-07-04 18:23:40 +0900

    separate image processing logic from main function

--- project_cls_mobilenet_v2/Main.cpp
+++ project_cls_mobilenet_v2/Main.cpp
@@ -1,73 +1,55 @@
+/*** Include ***/
+/* for general */
 #include <stdio.h>
 #include <vector>
 #include <algorithm>
 #include <chrono>
+
+/* for OpenCV */
 #include <opencv2/opencv.hpp>
-#include "net.h"
 
-#define MODEL_PARAM_NAME RESOURCE_DIR"/model/mobilenetv2-1.0.param"
-#define MODEL_BIN_NAME   RESOURCE_DIR"/model/mobilenetv2-1.0.bin"
-//#define MODEL_PARAM_NAME RESOURCE_DIR"/model/mobilenetv2-1.0_nobn.param"
-//#define MODEL_BIN_NAME   RESOURCE_DIR"/model/mobilenetv2-1.0_nobn.bin"
-//#define MODEL_PARAM_NAME RESOURCE_DIR"/model/mobilenetv2-1.0_int8.param"
-//#define MODEL_BIN_NAME   RESOURCE_DIR"/model/mobilenetv2-1.0_int8.bin"
-#define MODEL_WIDTH 224
-#define MODEL_HEIGHT 224
-#define MODEL_CHANNEL ncnn::Mat::PIXEL_BGR
-#define NCNN_CPU_NUM 4
+#include "ImageProcessor.h"
 
-#define LOOP_NUM_TO_MEASURE_INFERENCE_TIME 100
+/*** Macro ***/
+/* Model parameters */
+#define MODEL_NAME   RESOURCE_DIR"/model/mobilenetv2-1.0"
+#define LABEL_NAME   RESOURCE_DIR"/model/imagenet_labels.txt"
+#define IMAGE_NAME   RESOURCE_DIR"/parrot.jpg"
+
+/* Settings */
+#define LOOP_NUM_FOR_TIME_MEASUREMENT 10
 
 int main()
 {
-	/*** Load ncnn model (probably, need this only once) ***/
-	ncnn::Net net;
-	net.load_param(MODEL_PARAM_NAME);
-	net.load_model(MODEL_BIN_NAME);
+	/*** Initialize ***/
+	/* Initialize image processor library */
+	INPUT_PARAM inputParam;
+	strcpy_s(inputParam.labelFilename, sizeof(inputParam.labelFilename), LABEL_NAME);
+	ImageProcessor_initialize(MODEL_NAME, &inputParam);
 
-	/*** Read image using OpenCV ***/
-	cv::Mat image = cv::imread(RESOURCE_DIR"parrot.jpg");
-	cv::imshow("Display", image);
+	/* Read an input image */
+	cv::Mat originalImage = cv::imread(IMAGE_NAME);
 
-	/*** PreProcess for ncnn ***/
-	ncnn::Mat ncnnMat = ncnn::Mat::from_pixels_resize(image.data, MODEL_CHANNEL, image.cols, image.rows, MODEL_WIDTH, MODEL_HEIGHT);
-	float mean[3] = { 128.f, 128.f, 128.f };
-	float norm[3] = { 1 / 128.f, 1 / 128.f, 1 / 128.f };
-	ncnnMat.substract_mean_normalize(mean, norm);
+	/* Call image processor library */
+	OUTPUT_PARAM outputParam;
+	ImageProcessor_process(&originalImage, &outputParam);
 
-	/* Repeat inference */
-	/* the following loop is just to measure inference time, you don't need this for the real product */
-	std::vector<float> results;
-	auto t0 = std::chrono::system_clock::now();
-	for (int i = 0; i < LOOP_NUM_TO_MEASURE_INFERENCE_TIME; i++) {
-		/*** Prepare inference ***/
-		ncnn::Extractor ex = net.create_extractor();
-		ex.set_light_mode(true);
-		ex.set_num_threads(NCNN_CPU_NUM);
-		ex.input("data", ncnnMat);
+	cv::imshow("originalImage", originalImage);
+	cv::waitKey(1);
 
-		/*** Run inference ***/
-		ncnn::Mat ncnnOut;
-		ex.extract("mobilenetv20_output_flatten0_reshape0", ncnnOut);
-
-		/*** Retrieve results ***/
-		int outputNum = ncnnOut.w;
-		// std::vector<float> results(outputNum);
-		results.resize(outputNum);
-		for (int i = 0; i < outputNum; i++) {
-			results[i] = ((float*)ncnnOut.data)[i];
-		}
+	/*** (Optional) Measure inference time ***/
+	const auto& t0 = std::chrono::steady_clock::now();
+	for (int i = 0; i < LOOP_NUM_FOR_TIME_MEASUREMENT; i++) {
+		ImageProcessor_process(&originalImage, &outputParam);
 	}
-	auto t1 = std::chrono::system_clock::now();
+	const auto& t1 = std::chrono::steady_clock::now();
+	std::chrono::duration<double> timeSpan = t1 - t0;
+	printf("Inference time = %f [msec]\n", timeSpan.count() * 1000.0 / LOOP_NUM_FOR_TIME_MEASUREMENT);
 
-	/*** PostProcess ***/
-	int maxIndex = std::max_element(results.begin(), results.end()) - results.begin();
-	float maxScore = *std::max_element(results.begin(), results.end());
-	printf("Result = %d (%.3f)\n", maxIndex, maxScore);
-	
-	double inferenceTime = std::chrono::duration_cast<std::chrono::milliseconds>(t1-t0).count();
-	printf("Inference time: %.2lf [msec]\n", inferenceTime / LOOP_NUM_TO_MEASURE_INFERENCE_TIME);
+	/* Fianlize image processor library */
+	ImageProcessor_finalize();
 
-	cv::waitKey(0);
+	cv::waitKey(-1);
+
 	return 0;
 }
---
+++ project_det_mobilenet_v3/ImageProcessor/ImageProcessor.cpp
@@ -0,0 +1,162 @@
+/*** Include ***/
+/* for general */
+#include <stdio.h>
+#include <algorithm>
+#include <fstream>
+#include <functional>
+#include <memory>
+#include <sstream>
+#include <vector>
+#include <chrono>
+
+/* for OpenCV */
+#include <opencv2/opencv.hpp>
+
+/* for ncnn */
+#include "net.h"
+
+#include "ImageProcessor.h"
+
+/*** Macro ***/
+#if defined(ANDROID) || defined(__ANDROID__)
+#include <android/log.h>
+#define TAG "MyApp_NDK"
+#define PRINT(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
+#else
+#define PRINT(...) printf(__VA_ARGS__)
+#endif
+
+#define CHECK(x)                              \
+  if (!(x)) {                                                \
+	PRINT("Error at %s:%d\n", __FILE__, __LINE__); \
+	exit(1);                                                 \
+  }
+
+/* Settings */
+#define MODEL_WIDTH 300
+#define MODEL_HEIGHT 300
+#define MODEL_CHANNEL ncnn::Mat::PIXEL_BGR
+#define NCNN_CPU_NUM 4
+
+/* Types */
+typedef struct {
+	double x;
+	double y;
+	double w;
+	double h;
+	int classId;
+	std::string classIdName;
+	double score;
+} BBox;
+
+
+/*** Global variables ***/
+static ncnn::Net *s_net = NULL;
+static std::vector<std::string> s_labels;
+
+/*** Functions ***/
+static void getBBox(std::vector<BBox> &bboxList, const ncnn::Mat &ncnnOut, const double threshold, const int imageWidth = 0, const int imageHeight = 0)
+{
+	for (int i = 0; i < ncnnOut.h; i++) {
+		const float* values = ncnnOut.row(i);
+		BBox bbox;
+		bbox.classId = (int)values[0];
+		bbox.score = values[1];
+		if (bbox.score < threshold) continue;
+		bbox.x = std::max<float>(values[2], 0.0f);
+		bbox.y = std::max<float>(values[3], 0.0f);
+		bbox.w = std::min<float>(values[4], 1.0f) - values[2];
+		bbox.h = std::min<float>(values[5], 1.0f) - values[3];
+		if (imageWidth != 0) {
+			bbox.x *= imageWidth;
+			bbox.y *= imageHeight;
+			bbox.w *= imageWidth;
+			bbox.h *= imageHeight;
+		}
+		bboxList.push_back(bbox);
+	}
+}
+
+static void readLabel(const char* filename, std::vector<std::string> & labels)
+{
+	std::ifstream ifs(filename);
+	if (ifs.fail()) {
+		printf("failed to read %s\n", filename);
+		return;
+	}
+	std::string str;
+	while (getline(ifs, str)) {
+		labels.push_back(str);
+	}
+}
+
+
+int ImageProcessor_initialize(const char *modelFilename, INPUT_PARAM *inputParam)
+{
+	/* Load ncnn model */
+	s_net = new ncnn::Net();
+	CHECK(s_net != NULL);
+	CHECK(s_net->load_param((std::string(modelFilename) + ".param").c_str()) == 0);
+	CHECK(s_net->load_model((std::string(modelFilename) + ".bin").c_str()) == 0);
+
+	/* read label */
+	readLabel(inputParam->labelFilename, s_labels);
+
+	return 0;
+}
+
+
+int ImageProcessor_process(cv::Mat *mat, OUTPUT_PARAM *outputParam)
+{
+	/*** PreProcess for ncnn ***/
+	ncnn::Mat ncnnMat = ncnn::Mat::from_pixels_resize(mat->data, MODEL_CHANNEL, mat->cols, mat->rows, MODEL_WIDTH, MODEL_HEIGHT);
+	float mean[3] = { 123.675f, 116.28f, 103.53f };
+	float norm[3] = { 1.0f, 1.0f, 1.0f };
+	ncnnMat.substract_mean_normalize(mean, norm);
+
+	/*** Inference ***/
+	ncnn::Extractor ex = s_net->create_extractor();
+	ex.set_light_mode(true);
+	ex.set_num_threads(NCNN_CPU_NUM);
+	CHECK(ex.input("input", ncnnMat) == 0);
+
+	/*** Run inference ***/
+	ncnn::Mat ncnnOut;
+	CHECK(ex.extract("detection_out", ncnnOut) == 0);
+
+	/*** PostProcess ***/
+	/* Retrieve results */
+	std::vector<BBox> bboxList;
+	getBBox(bboxList, ncnnOut, 0.2, mat->cols, mat->rows);
+	
+	/* Display bbox */
+	for (int i = 0; i < bboxList.size(); i++) {
+		const BBox bbox = bboxList[i];
+		cv::rectangle(*mat, cv::Rect((int)bbox.x, (int)bbox.y, (int)bbox.w, (int)bbox.h), cv::Scalar(255, 255, 0), 3);
+		cv::putText(*mat, s_labels[bbox.classId], cv::Point((int)bbox.x, (int)bbox.y + 10), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 0, 0), 3);
+		cv::putText(*mat, s_labels[bbox.classId], cv::Point((int)bbox.x, (int)bbox.y + 10), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 1);
+	}
+
+	/* Contain the results */
+	outputParam->resultNum = (int)bboxList.size();
+	if (outputParam->resultNum > NUM_MAX_RESULT) outputParam->resultNum = NUM_MAX_RESULT;
+	for (int i = 0; i < outputParam->resultNum; i++) {
+		const BBox bbox = bboxList[i];
+		outputParam->RESULTS[i].classId = bbox.classId;
+		strcpy_s(outputParam->RESULTS[i].label, sizeof(outputParam->RESULTS[i].label), s_labels[bbox.classId].c_str());
+		outputParam->RESULTS[i].score = bbox.score;
+		outputParam->RESULTS[i].x = (int)bbox.x;
+		outputParam->RESULTS[i].y = (int)bbox.y;
+		outputParam->RESULTS[i].width = (int)bbox.w;
+		outputParam->RESULTS[i].height = (int)bbox.h;
+	}
+	
+	return 0;
+}
+
+
+int ImageProcessor_finalize(void)
+{
+	delete s_net;
+	return 0;
+}
