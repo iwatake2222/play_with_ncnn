c37cb19b020480dc35324a64e538560bdb0850c9
take-iwiw <take.iwiw2222@gmail.com>
2020-11-23 03:07:07 +0900

    Updated detection project

--- pj_ncnn_det_mobilenet_v3/ImageProcessor/ImageProcessor.cpp
+++ pj_ncnn_det_mobilenet_v3/ImageProcessor/ImageProcessor.cpp
@@ -1,164 +1,130 @@
 /*** Include ***/
 /* for general */
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdint>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
 #include <string>
-#include <algorithm>
-#include <fstream>
-#include <functional>
-#include <memory>
-#include <sstream>
 #include <vector>
+#include <array>
+#include <algorithm>
 #include <chrono>
+#include <fstream>
+#include <memory>
 
 /* for OpenCV */
 #include <opencv2/opencv.hpp>
 
-/* for ncnn */
-#include "net.h"
-
+/* for My modules */
+#include "CommonHelper.h"
+#include "DetectionEngine.h"
 #include "ImageProcessor.h"
 
 /*** Macro ***/
-#if defined(ANDROID) || defined(__ANDROID__)
-#include <android/log.h>
-#define TAG "MyApp_NDK"
-#define PRINT(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
+#define TAG "ImageProcessor"
+#define PRINT(...)   COMMON_HELPER_PRINT(TAG, __VA_ARGS__)
+#define PRINT_E(...) COMMON_HELPER_PRINT_E(TAG, __VA_ARGS__)
+
+/*** Global variable ***/
+std::unique_ptr<DetectionEngine> s_classificationEngine;
+
+/*** Function ***/
+static cv::Scalar createCvColor(int32_t b, int32_t g, int32_t r) {
+#ifdef CV_COLOR_IS_RGB
+	return cv::Scalar(r, g, b);
 #else
-#define PRINT(...) printf(__VA_ARGS__)
+	return cv::Scalar(b, g, r);
 #endif
-
-#define CHECK(x)                              \
-  if (!(x)) {                                                \
-	PRINT("Error at %s:%d\n", __FILE__, __LINE__); \
-	exit(1);                                                 \
-  }
-
-/* Settings */
-#define MODEL_WIDTH 300
-#define MODEL_HEIGHT 300
-#define MODEL_CHANNEL ncnn::Mat::PIXEL_BGR
-#define NCNN_CPU_NUM 4
-
-/* Types */
-typedef struct {
-	double x;
-	double y;
-	double w;
-	double h;
-	int classId;
-	std::string classIdName;
-	double score;
-} BBox;
-
-
-/*** Global variables ***/
-static ncnn::Net *s_net = NULL;
-static std::vector<std::string> s_labels;
-
-/*** Functions ***/
-static void getBBox(std::vector<BBox> &bboxList, const ncnn::Mat &ncnnOut, const double threshold, const int imageWidth = 0, const int imageHeight = 0)
-{
-	for (int i = 0; i < ncnnOut.h; i++) {
-		const float* values = ncnnOut.row(i);
-		BBox bbox;
-		bbox.classId = (int)values[0];
-		bbox.score = values[1];
-		if (bbox.score < threshold) continue;
-		bbox.x = std::max<float>(values[2], 0.0f);
-		bbox.y = std::max<float>(values[3], 0.0f);
-		bbox.w = std::min<float>(values[4], 1.0f) - values[2];
-		bbox.h = std::min<float>(values[5], 1.0f) - values[3];
-		if (imageWidth != 0) {
-			bbox.x *= imageWidth;
-			bbox.y *= imageHeight;
-			bbox.w *= imageWidth;
-			bbox.h *= imageHeight;
-		}
-		bboxList.push_back(bbox);
-	}
-}
-
-static void readLabel(const char* filename, std::vector<std::string> & labels)
-{
-	std::ifstream ifs(filename);
-	if (ifs.fail()) {
-		PRINT("failed to read %s\n", filename);
-		return;
-	}
-	std::string str;
-	while (getline(ifs, str)) {
-		labels.push_back(str);
-	}
 }
 
 
-int ImageProcessor_initialize(const char *modelFilename, INPUT_PARAM *inputParam)
+int32_t ImageProcessor_initialize(const INPUT_PARAM* inputParam)
 {
-	/* Load ncnn model */
-	s_net = new ncnn::Net();
-	CHECK(s_net != NULL);
-	CHECK(s_net->load_param((std::string(modelFilename) + ".param").c_str()) == 0);
-	CHECK(s_net->load_model((std::string(modelFilename) + ".bin").c_str()) == 0);
+	if (s_classificationEngine) {
+		PRINT_E("Already initialized\n");
+		return -1;
+	}
 
-	/* read label */
-	readLabel(inputParam->labelFilename, s_labels);
+	s_classificationEngine.reset(new DetectionEngine());
+	if (s_classificationEngine->initialize(inputParam->workDir, inputParam->numThreads) != DetectionEngine::RET_OK) {
+		s_classificationEngine->finalize();
+		s_classificationEngine.reset();
+		return -1;
+	}
+	return 0;
+}
+
+int32_t ImageProcessor_finalize(void)
+{
+	if (!s_classificationEngine) {
+		PRINT_E("Not initialized\n");
+		return -1;
+	}
+
+	if (s_classificationEngine->finalize() != DetectionEngine::RET_OK) {
+		return -1;
+	}
 
 	return 0;
 }
 
 
-int ImageProcessor_process(cv::Mat *mat, OUTPUT_PARAM *outputParam)
+int32_t ImageProcessor_command(int32_t cmd)
 {
-	/*** PreProcess for ncnn ***/
-	ncnn::Mat ncnnMat = ncnn::Mat::from_pixels_resize(mat->data, MODEL_CHANNEL, mat->cols, mat->rows, MODEL_WIDTH, MODEL_HEIGHT);
-	float mean[3] = { 123.675f, 116.28f, 103.53f };
-	float norm[3] = { 1.0f, 1.0f, 1.0f };
-	ncnnMat.substract_mean_normalize(mean, norm);
-
-	/*** Inference ***/
-	ncnn::Extractor ex = s_net->create_extractor();
-	ex.set_light_mode(true);
-	ex.set_num_threads(NCNN_CPU_NUM);
-	CHECK(ex.input("input", ncnnMat) == 0);
-
-	/*** Run inference ***/
-	ncnn::Mat ncnnOut;
-	CHECK(ex.extract("detection_out", ncnnOut) == 0);
-
-	/*** PostProcess ***/
-	/* Retrieve results */
-	std::vector<BBox> bboxList;
-	getBBox(bboxList, ncnnOut, 0.2, mat->cols, mat->rows);
-	
-	/* Display bbox */
-	for (int i = 0; i < (int)bboxList.size(); i++) {
-		const BBox bbox = bboxList[i];
-		cv::rectangle(*mat, cv::Rect((int)bbox.x, (int)bbox.y, (int)bbox.w, (int)bbox.h), cv::Scalar(255, 255, 0), 3);
-		cv::putText(*mat, s_labels[bbox.classId], cv::Point((int)bbox.x, (int)bbox.y + 10), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 0, 0), 3);
-		cv::putText(*mat, s_labels[bbox.classId], cv::Point((int)bbox.x, (int)bbox.y + 10), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 1);
+	if (!s_classificationEngine) {
+		PRINT_E("Not initialized\n");
+		return -1;
 	}
 
-	/* Contain the results */
-	outputParam->resultNum = (int)bboxList.size();
-	if (outputParam->resultNum > NUM_MAX_RESULT) outputParam->resultNum = NUM_MAX_RESULT;
-	for (int i = 0; i < outputParam->resultNum; i++) {
-		const BBox bbox = bboxList[i];
-		outputParam->RESULTS[i].classId = bbox.classId;
-		snprintf(outputParam->RESULTS[i].label, sizeof(outputParam->RESULTS[i].label), "%s", s_labels[bbox.classId].c_str());
-		outputParam->RESULTS[i].score = bbox.score;
-		outputParam->RESULTS[i].x = (int)bbox.x;
-		outputParam->RESULTS[i].y = (int)bbox.y;
-		outputParam->RESULTS[i].width = (int)bbox.w;
-		outputParam->RESULTS[i].height = (int)bbox.h;
+	switch (cmd) {
+	case 0:
+	default:
+		PRINT_E("command(%d) is not supported\n", cmd);
+		return -1;
 	}
-	
-	return 0;
 }
 
 
-int ImageProcessor_finalize(void)
+int32_t ImageProcessor_process(cv::Mat* mat, OUTPUT_PARAM* outputParam)
 {
-	delete s_net;
+	if (!s_classificationEngine) {
+		PRINT_E("Not initialized\n");
+		return -1;
+	}
+
+	const cv::Mat originalMat = *mat;
+	DetectionEngine::RESULT result;
+	result.objectList.clear();
+	if (s_classificationEngine->invoke(originalMat, result) != DetectionEngine::RET_OK) {
+		return -1;
+	}
+
+	/* Draw the result */
+	for (const auto& object : result.objectList) {
+		cv::rectangle(originalMat, cv::Rect(static_cast<int32_t>(object.x), static_cast<int32_t>(object.y), static_cast<int32_t>(object.width), static_cast<int32_t>(object.height)), cv::Scalar(255, 255, 0), 3);
+		cv::putText(originalMat, object.label, cv::Point(static_cast<int32_t>(object.x), static_cast<int32_t>(object.y) + 10), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 0, 0), 3);
+		cv::putText(originalMat, object.label, cv::Point(static_cast<int32_t>(object.x), static_cast<int32_t>(object.y) + 10), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 255, 0), 1);
+	}
+
+
+	/* Return the results */
+	int32_t objectNum = 0;
+	for (const auto& object : result.objectList) {
+		outputParam->objectList[objectNum].classId = object.classId;
+		snprintf(outputParam->objectList[objectNum].label, sizeof(outputParam->objectList[objectNum].label), "%s", object.label.c_str());
+		outputParam->objectList[objectNum].score = object.score;
+		outputParam->objectList[objectNum].x = static_cast<int32_t>(object.x);
+		outputParam->objectList[objectNum].y = static_cast<int32_t>(object.y);
+		outputParam->objectList[objectNum].width = static_cast<int32_t>(object.width);
+		outputParam->objectList[objectNum].height = static_cast<int32_t>(object.height);
+		objectNum++;
+		if (objectNum >= NUM_MAX_RESULT) break;
+	}
+	outputParam->objectNum = objectNum;
+	outputParam->timePreProcess = result.timePreProcess;
+	outputParam->timeInference = result.timeInference;
+	outputParam->timePostProcess = result.timePostProcess;
+
 	return 0;
 }
+
